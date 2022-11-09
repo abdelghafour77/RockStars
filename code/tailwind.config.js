70bb15896b57611a20f4a19b769fdbdc8f5b41d0
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2022-11-09 17:11:04 +0100

    create responsive landing page using Tailwind css

---
+++ code/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      padding: '2rem',
+    },
+    fontFamily: {
+      'RockStars': ['Rock Salt, cursive'],
+      'PoorStory': ['Poor Story, cursive'],
+      'Poppins': ['Poppins, sans-serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      // lg: '976px',
+      lg: '1200px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        gold: '#e3b04b',
+        lightGold: '#ffcd69',
+        transparentBlack: '#00000066',
+      }
+    },
+  },
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
+}
